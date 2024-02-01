117d4bb90c1f79b8e4abfcf48aded818e57ceae6
buijennyh <109253137+buijennyh@users.noreply.github.com>
2024-02-01 01:26:13 -0600

    Apply styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '../assets/logo.png'
 import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#A087BC] text-slate-900'>Navbar
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#D9CCE8] text-slate-900'>Navbar
        
         <div>
             <img src={Logo} alt="Logo Image" style={{width: '80px'}} />
         </div>
+ 
+       {/* Menu */}
 
-        <div>
-            <ul className='flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Experience</li>
-                <li>Contact</li>
-
-            </ul>
-        </div>
+         <ul className='md:flex'>
+             <li>Home</li>
+             <li>About</li>
+             <li>Experience</li>
+             <li>Contact</li>
+         </ul>
+        
 
        {/* Hamburger */}
-        <div className='hidden flex'>
-            <FaBars />
+        <div onClick={handleClick} className='md:flex z-10'>
+            {!nav ? <FaBars /> : <FaTimes />}
         </div>
 
        {/* Mobile Menu */}
-       <ul className='hidden'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Experience and Work</li>
-            <li>Contact</li>
+       <ul className={!nav ? 'hidden' : 'absolute top-0 left-1 w-full h-screen bg-[#A087B9] flex flex-column justify-center items-center'}>
+            <li className='py-6 text-4xl'>Home</li>
+            <li className='py-6 text-4xl'>About</li>
+            <li className='py-6 text-4xl'>Experience</li>
+            <li className='py-6 text-4xl'>Contact</li>
         </ul>
 
        {/*Social Icons */} 
-    {/*BOOKMARK- 16:15 URL:https://www.youtube.com/watch?v=2kg0z1qNrkw}*/}
-
-        <div></div>
-
+        <div className='hidden'></div>
     </div>
   )
 }
