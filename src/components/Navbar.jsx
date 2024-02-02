478a79fd7da981ca39052f1e56de2f3481040a87
buijennyh <109253137+buijennyh@users.noreply.github.com>
2024-02-01 23:43:37 -0600

    Added handleClick function for Menu items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,13 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav)
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#D9CCE8] text-slate-900'>Navbar
-       
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#D9CCE8] text-slate-900'>
         <div>
-            <img src={Logo} alt="Logo Image" style={{width: '80px'}} />
+            <img src={Logo} alt='Logo Image' style={{width: '80px'}} />
         </div>
  
        {/* Menu */}
-
-         <ul className='md:flex'>
+         <ul className='hidden md:flex'>
              <li>Home</li>
              <li>About</li>
              <li>Experience</li>
@@ -24,18 +22,19 @@ const Navbar = () => {
         
 
        {/* Hamburger */}
-        <div onClick={handleClick} className='md:flex z-10'>
+        <div onClick={handleClick} className='md:hidden z-10'>
             {!nav ? <FaBars /> : <FaTimes />}
         </div>
 
        {/* Mobile Menu */}
-       <ul className={!nav ? 'hidden' : 'absolute top-0 left-1 w-full h-screen bg-[#A087B9] flex flex-column justify-center items-center'}>
+       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#A087B9] flex flex-col justify-center items-center'}>
             <li className='py-6 text-4xl'>Home</li>
             <li className='py-6 text-4xl'>About</li>
             <li className='py-6 text-4xl'>Experience</li>
             <li className='py-6 text-4xl'>Contact</li>
         </ul>
 
+{/*STOPPED HERE 22:50 URL: https://www.youtube.com/watch?v=2kg0z1qNrkw*/}
        {/*Social Icons */} 
         <div className='hidden'></div>
     </div>
