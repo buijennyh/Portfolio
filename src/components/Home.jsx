b97ee68e3029d647e83d9d4e07c750ccd8b1cb17
buijennyh <109253137+buijennyh@users.noreply.github.com>
2024-02-02 00:25:35 -0600

    add social media bars with hidden:flex and added desired color palette

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen bg-[#efe7f7] text-slate-900'>Home</div>
+  )
+}
+
+{/*STOPPED HERE 32:50 URL: https://www.youtube.com/watch?v=2kg0z1qNrkw*/}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import Logo from '../assets/logo.png'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from 'react-icons/fa'
+import {HiOutLineMail} from 'react-icons/hi'
+import { BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -34,9 +36,21 @@ const Navbar = () => {
             <li className='py-6 text-4xl'>Contact</li>
         </ul>
 
-{/*STOPPED HERE 22:50 URL: https://www.youtube.com/watch?v=2kg0z1qNrkw*/}
        {/*Social Icons */} 
-        <div className='hidden'></div>
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+          <ul>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#5d94b2]'>
+              <a className='w-full flex justify-between items-center text-gray-100' href="/">
+                Linkedin <FaLinkedin size={30} />
+              </a>
+            </li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#767474]'>
+              <a className='w-full flex justify-between items-center text-gray-100' href="/">
+               GitHub <FaGithub size={30} />
+              </a>
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }
