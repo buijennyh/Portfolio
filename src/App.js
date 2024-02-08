74e001e21f7db740406cafaafdcc5d6539f9937c
buijennyh <109253137+buijennyh@users.noreply.github.com>
2024-02-07 19:06:04 -0600

    Experience component created and img testing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Experience from "./components/Experience";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Experience />
     </div>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import GitHub from '../assets/github.png'
+import HTML from '../assets/html.png'
+import JavaScript from '../assets/javascript.png'
+import ReactImg from '../assets/react.png'
+import Node from '../assets/node.png'
+import Mongo from '../assets/mongo.png'
+
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div name='experience'>
+        {/* container */}
+        <div>
+            <div>
+                <p> Experience </p> 
+                <p>I have worked with:</p>
+            </div>
+            <div>
+                <div>
+                    <img src={HTML} alt="HTML icon" />
+                    <p>HTML</p>
+                </div>
+            </div>
+
+        </div>
+    
+    </div>
   )
 }
 
