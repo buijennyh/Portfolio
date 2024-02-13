10a9588f07dbca47681761ec90bff58b40dc3bd7
buijennyh <109253137+buijennyh@users.noreply.github.com>
2024-02-12 21:06:46 -0600

    removed shadow from skill icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -19,27 +19,27 @@ const Experience = () => {
                 <p className='py-4'> Technologies worked with:</p>
             </div>
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
-                <div className='shadow-md shadow-[#040C16] hover:scale-110 duration-500'>
+                <div className='hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                     <p className='my-4'>HTML</p>
                 </div>
-                <div className='shadow-md shadow-[#040C16] hover:scale-110 duration-500'>
+                <div className='hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
                     <p className='my-4'>CSS</p>
                 </div>
-                <div className='shadow-md shadow-[#040C16] hover:scale-110 duration-500'>
+                <div className=' hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript icon" />
                     <p className='my-4'>JavaScript</p>
                 </div>
-                <div className='shadow-md shadow-[#040C16] hover:scale-110 duration-500'>
+                <div className='hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={Java} alt="Java icon" />
                     <p className='my-4'>Java</p>
                 </div>
-                <div className='shadow-md shadow-[#040C16] hover:scale-110 duration-500'>
+                <div className='hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
                     <p className='my-4'>React</p>
                 </div>
-                <div className='shadow-md shadow-[#040C16] hover:scale-110 duration-500'>
+                <div className='hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={Node} alt="Node icon" />
                     <p className='my-4'>Node</p>
                 </div>
