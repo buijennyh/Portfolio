05dd660694b1642c87ca8d8798eb93174db9763f
buijennyh <109253137+buijennyh@users.noreply.github.com>
2024-02-15 13:51:25 -0600

    New Color Scheme Alertgit add --a Updated colors on bg and text-color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,17 +3,17 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#1f102f] text-slate-900'>
+    <div name='home' className='w-full h-screen bg-[#F0F2DC] text-slate-900'>
 
       {/*Container*/}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-[#9C76D6]'>Welcome, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-purple-800'> Jenny B </h1>
-        <h2 className='text-4xl sm-text-7xl font-bold text-[#f3eb64]'>Full-Stack Developer</h2> 
-        <p className='text-[#8892B0] py-4 mac-w-[700px'>Discription: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum dolore optio praesentium vitae porro facere doloremque, quo repellendus maxime voluptas eos ratione enim assumenda, autem consequatur debitis expedita quos quas?</p>
+        <p className='text-[#8C8274]'>Welcome, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#260906]'> Jenny B </h1>
+        <h2 className='text-4xl sm-text-7xl font-bold text-[#8C8274]'>Full-Stack Developer</h2> 
+        <p className='text-[#260906] py-4 mac-w-[700px'>Discription: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum dolore optio praesentium vitae porro facere doloremque, quo repellendus maxime voluptas eos ratione enim assumenda, autem consequatur debitis expedita quos quas?</p>
      
         <div>
-          <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-purple-800 hover:border-purple-800'>View Work <HiArrowNarrowRight className='ml-3'/>
+          <button className='text-[#260906] border-4 px-6 py-3 my-2 flex items-center hover:bg-[#8C8274] hover:border-[#402C25]'>View Work <HiArrowNarrowRight className='ml-3'/>
           </button>  
         </div>
       </div>
@@ -22,6 +22,6 @@ const Home = () => {
   )
 }
 
-{/*STOPPED HERE 60:00 URL: https://www.youtube.com/watch?v=2kg0z1qNrkw*/}
+{/*STOPPED HERE 1:08:00 URL: https://www.youtube.com/watch?v=2kg0z1qNrkw*/}
 
 export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav)
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#e1dae8] text-slate-900'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#D6D9C5] text-slate-900'>
         <div>
             <img src={Logo} alt='Logo Image' style={{width: '80px'}} />
         </div>
@@ -29,7 +29,7 @@ const Navbar = () => {
         </div>
 
        {/* Mobile Menu */}
-       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#A087B9] flex flex-col justify-center items-center'}>
+       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#D6D9C5] flex flex-col justify-center items-center'}>
             <li className='py-6 text-4xl'>Home</li>
             <li className='py-6 text-4xl'>About</li>
             <li className='py-6 text-4xl'>Experience</li>
@@ -40,12 +40,12 @@ const Navbar = () => {
         <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
           <ul>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#5d94b2]'>
-              <a className='w-full flex justify-between items-center text-gray-100' href="/">
+              <a className='w-full flex justify-between items-center text-[#260906]' href="/">
                 Linkedin <FaLinkedin size={30} />
               </a>
             </li>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#767474]'>
-              <a className='w-full flex justify-between items-center text-gray-100' href="/">
+              <a className='w-full flex justify-between items-center text-[#260906]' href="/">
                GitHub <FaGithub size={30} />
               </a>
             </li>
